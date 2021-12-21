09c976b42ff37086763cde1d7d4ac817ed776500
livefrugally <livefrugally@naver.com>
2021-12-21 21:48:46 +0900

    단계별 문제풀이 10171 고양이

---
+++ src/단계별로풀어보기/Q10171.java
@@ -0,0 +1,16 @@
+package 단계별로풀어보기;
+
+import java.io.IOException;
+
+/*고양이
+풀이설명 : \ 백 슬래쉬는 두번해야 \로 인식한다. 안그럼 오류.
+*/
+public class Q10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
