178751b4bc449c8224a3ae6c2bb8feeaac73fb5b
thkim9012 <cesar@kakao.com>
2021-12-23 10:47:55 +0900

    수정

--- src/단계별로풀어보기/Q10171.java
+++ src/단계별로풀어보기/Q10171.java
@@ -1,10 +1,11 @@
 package 단계별로풀어보기;
 
-import java.io.IOException;
-
-/*고양이
-풀이설명 : \ 백 슬래쉬는 두번해야 \로 인식한다. 안그럼 오류.
-*/
+/**
+ * \    /\
+ *  )  ( ')
+ * (  /  )
+ *  \(__)|
+ */
 public class Q10171 {
 
     public static void main(String[] args) {
